314da41d7dd39d1cf1443c17e7a38ea6b33eda7e
Twesh <66848727+ParkWardRR@users.noreply.github.com>
2026-06-28 13:31:46 -0700

    feat: prime-time readiness, standalone cargo and docs

---
+++ target/debug/deps/alac_encoder_rs_lucianari-b6db22f935a8c636.d
@@ -0,0 +1,12 @@
+/Users/twesh/Antigravity/spinoffs/alac-encoder-rs-lucianari/target/debug/deps/alac_encoder_rs_lucianari-b6db22f935a8c636.d: src/lib.rs src/bitwriter.rs src/encoder.rs src/golomb.rs src/predictor.rs src/simd.rs
+
+/Users/twesh/Antigravity/spinoffs/alac-encoder-rs-lucianari/target/debug/deps/libalac_encoder_rs_lucianari-b6db22f935a8c636.rlib: src/lib.rs src/bitwriter.rs src/encoder.rs src/golomb.rs src/predictor.rs src/simd.rs
+
+/Users/twesh/Antigravity/spinoffs/alac-encoder-rs-lucianari/target/debug/deps/libalac_encoder_rs_lucianari-b6db22f935a8c636.rmeta: src/lib.rs src/bitwriter.rs src/encoder.rs src/golomb.rs src/predictor.rs src/simd.rs
+
+src/lib.rs:
+src/bitwriter.rs:
+src/encoder.rs:
+src/golomb.rs:
+src/predictor.rs:
+src/simd.rs:
---
+++ target/debug/deps/alac_encoder_rs_lucianari-f2ba4bb53f9d14dd.d
@@ -0,0 +1,10 @@
+/Users/twesh/Antigravity/spinoffs/alac-encoder-rs-lucianari/target/debug/deps/alac_encoder_rs_lucianari-f2ba4bb53f9d14dd.d: src/lib.rs src/bitwriter.rs src/encoder.rs src/golomb.rs src/predictor.rs src/simd.rs
+
+/Users/twesh/Antigravity/spinoffs/alac-encoder-rs-lucianari/target/debug/deps/alac_encoder_rs_lucianari-f2ba4bb53f9d14dd: src/lib.rs src/bitwriter.rs src/encoder.rs src/golomb.rs src/predictor.rs src/simd.rs
+
+src/lib.rs:
+src/bitwriter.rs:
+src/encoder.rs:
+src/golomb.rs:
+src/predictor.rs:
+src/simd.rs:
